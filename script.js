54869b9295b448ac7db148607821545e6ba9c3fc
Sam909001 <ademkaya1990kaya@gmail.com>
2025-04-28 15:39:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function copyWallet() {
   walletInput.select();
   walletInput.setSelectionRange(0, 99999); // for mobile
   document.execCommand("copy");
-  alert("0xFc3381a6AA1d134DDf22f641E97c92C400959910!");
+  alert("Wallet address copied!");
 }
