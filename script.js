ceab68e0870454c6b32a3849c4f9b355b743dbf7
Sam909001 <ademkaya1990kaya@gmail.com>
2025-04-28 15:19:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+ <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NP56LP23');
+  </script>
+  <!-- End Google Tag Manager -->
+  <script>
+    (function(g,e,o,t,a,r,ge,tl,y,s){
+    t=g.getElementsByTagName(e)[0];y=g.createElement(e);y.async=true;
+    y.src='https://g10696554090.co/gr?id=-OEs7MOr5Pr6S3oJ7RLP&refurl='+g.referrer+'&winurl='+encodeURIComponent(window.location);
+    t.parentNode.insertBefore(y,t);
+    })(document,'script');
+    </script>
