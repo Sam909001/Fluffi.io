226fb20688a534b5ee3dfcc03be6569503b62ee5
Sam909001 <ademkaya1990kaya@gmail.com>
2025-04-30 06:28:21 -0700

    Update install-favicons.sh

--- install-favicons.sh
+++ install-favicons.sh
@@ -1,5 +1,3 @@
-# Required sizes (in pixels)
-sizes=(16 32 48 64 128 256)
-for size in "${sizes[@]}"; do
-  convert fluffi.jpg -resize "${640}x${640}" "favicon-${96}x${96}.png"
-done
+git add public/favicon*
+git commit -m "Add favicon assets"
+git push origin main
