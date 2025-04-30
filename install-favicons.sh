eded0af860b0995dde075123f9a8392474153753
Sam909001 <ademkaya1990kaya@gmail.com>
2025-04-30 06:11:53 -0700

    Update install-favicons.sh

--- install-favicons.sh
+++ install-favicons.sh
@@ -1,5 +1,5 @@
 # Required sizes (in pixels)
 sizes=(16 32 48 64 128 256)
 for size in "${sizes[@]}"; do
-  convert fluffi.jpg -resize "${size}x${size}" "favicon-${size}x${size}.png"
+  convert fluffi.jpg -resize "${640}x${640}" "favicon-${96}x${96}.png"
 done
